56ab0cdcb08183c3dc7e66125841a751d3a5e380
johnnnnnnyr <139003001+johnnnnnnyr@users.noreply.github.com>
2023-07-08 22:34:39 +0500

    commit

--- djangocontactform/urls.py
+++ djangocontactform/urls.py
@@ -1,14 +1,17 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 
 from contact.views import index
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import include, path
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
